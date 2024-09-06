bef19a1964ec43223785d8e881e4b72d0b640eec
yukihito <ok220020@oks.iput.ac.jp>
2024-09-06 09:30:22 +0900

    単語辞書を作成するメソッドの作成

--- src/transformers_scratch/Token.py
+++ src/transformers_scratch/Token.py
@@ -3,16 +3,7 @@ import json
 
 class Token:
     """
-    method
-    ----------
-    save()
-        単語と単語IDを保存する関数。
-    to_id(list[str]) -> list[int]
-        単語から単語IDに変換する関数。
-    to_toekn(list[int]) -> list[str]
-        単語IDから単語に変換する関数。
-
-    Attributes
+        Attributes
     ----------
     self.path : str
         単語の辞書を保存するjsonファイルのpath
@@ -20,6 +11,18 @@ class Token:
         単語とそれに対する単語IDが保存されている。
     self.id2token : dict[int, str]
         単語IDとそれに対する単語が保存されている。
+
+    method
+    ----------
+    make_vocab(path: str)
+        単語の辞書を作成する関数。
+        トークンに分割されたtextファイルから作成される。
+    save()
+        単語と単語IDを保存する関数。
+    to_id(list[str]) -> list[int]
+        単語から単語IDに変換する関数。
+    to_toekn(list[int]) -> list[str]
+        単語IDから単語に変換する関数。
     """
 
     def __init__(self, path: str) -> None:
@@ -47,5 +50,31 @@ class Token:
             self.token2id = {}
             self.id2token = {}
 
-    def make_vocab(self) -> None:
-        pass
+    def make_vocab(self, path: str) -> None:
+        """
+        説明
+        ----------
+        単語辞書を作成するメソッド
+
+        Parameters
+        ----------
+        path : str
+            tokenに分割されたtextファイル
+        """
+
+        # ファイルの読み込み
+        try:
+            with open(path, "r", encoding="utf-8") as file:
+                text = file.read()
+        except FileNotFoundError:
+            print(f"Warning: The file '{path}' was not found.")
+            return
+
+        word_list = text.split(" ")
+
+        # 単語辞書の作成
+        for word in word_list:
+            if word not in self.token2id:
+                idx = str(len(self.token2id))
+                self.token2id[word] = idx
+                self.id2token[idx] = word
